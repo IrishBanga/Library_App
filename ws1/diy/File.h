aa2d57aa5140230dcc9f18131060efe7358a277b
Irish Banga <ibanga1@myseneca.ca>
2023-05-18 08:37:19 -0400

    w1p2

---
+++ ws1/diy/File.h
@@ -0,0 +1,26 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+Completion Date: 2023-05-18
+
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#ifndef SDDS_FILE_H
+#define SDDS_FILE_H
+
+namespace sdds
+{
+	struct ShoppingRec;
+	bool openFileForRead();
+	bool openFileForOverwrite();
+	void closeFile();
+	bool freadShoppingRec(ShoppingRec* rec);
+	void fwriteShoppintRec(const ShoppingRec* rec);
+}
+#endif // !SDDS_FILE_H
+
+
+
+
