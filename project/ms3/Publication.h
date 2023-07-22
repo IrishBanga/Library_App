f96a9191b38136657a59f5bb0306daa10ffb1147
IRISH BANGA <122631145+IrishBanga@users.noreply.github.com>
2023-07-21 22:32:02 -0400

    MS3

---
+++ project/ms3/Publication.h
@@ -0,0 +1,60 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+
+Citation and Sources...
+Final Project Milestone 3
+Module: Publication
+Filename: Publication.h
+Version 1.0
+Author: Irish Banga
+Revision History
+-----------------------------------------------------------
+Date        Reason
+2023/07/21  Preliminary release
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my project milestones.
+-----------------------------------------------------------
+
+*/
+#ifndef SDDS_PUBLICATION_H
+#define SDDS_PUBLICATION_H
+#include "Streamable.h"
+#include "Date.h"
+#include "Lib.h"
+namespace sdds
+{
+	class Publication :
+		    public Streamable
+	{
+        char* m_title{ nullptr };
+        char m_shelfId[SDDS_SHELF_ID_LEN + 1]{};
+        int m_membership{ 0 };
+        int m_libRef{ -1 };
+        Date m_date;
+
+    public:
+        Publication() {};
+        Publication(const Publication& orignial);
+        Publication& operator=(const Publication& original);
+        ~Publication();
+
+        virtual void set(int member_id);
+        void setRef(int value);
+        void resetDate();
+        virtual char type()const;
+        bool onLoan()const;
+        Date checkoutDate()const;
+        bool operator==(const char* title)const;
+        operator const char* ()const;
+        int getRef()const;
+
+        bool conIO(const std::ios& io) const;
+        std::ostream& write(std::ostream& os) const;
+        std::istream& read(std::istream& istr);
+        operator bool() const;
+    };
+}
+#endif // !SDDS_PUBLICATION_H
---
+++ project/ms3/Streamable.cpp
@@ -0,0 +1,38 @@
+/*
+Name: Irish Banga
+Email: ibanga1@myseneca.ca
+Student ID: 112435227
+
+Citation and Sources...
+Final Project Milestone 3
+Module: Streamable
+Filename: Streamable.cpp
+Version 1.0
+Author: Irish Banga
+Revision History
+-----------------------------------------------------------
+Date        Reason
+2023/07/21  Preliminary release
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my project milestones.
+-----------------------------------------------------------
+
+*/
+#include "Streamable.h"
+namespace sdds
+{
+	std::ostream& operator<<(std::ostream& ostr, const Streamable& display)
+	{
+		if (display)
+		{
+			display.write(ostr);
+		}
+		return ostr;
+	}
+	std::istream& operator>>(std::istream& istr, Streamable& read)
+	{
+		read.read(istr);
+		return istr;
+	}
+}
